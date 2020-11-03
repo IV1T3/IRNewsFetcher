b81bd41eaf288beed56eca256c426f3ced874239
IV1T3 <alexander.groddeck@studium.uni-hamburg.de>
2020-11-03 19:45:40 +0100

    Removed helper.py

--- Company.py
+++ Company.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup
-import helper
+import requests
 
 # Tesla information
 tesla_url_main = "https://ir.tesla.com"
@@ -7,24 +7,42 @@ tesl_url_press = "https://ir.tesla.com/press"
 tesla_main_id = "main-content"
 tesla_press_releases = ["section", "class", "press-release-teaser"]
 tesla_press_release_title = ["h4", "class", "press-release-teaser__title", "a"]
+tesla_press_release_date = [
+    "div",
+    "class",
+    "press-release-teaser__date tds-text--caption tds-text_color--35",
+    "time",
+]
 
 
 class Company:
     def __init__(self, name: str) -> None:
         self.name = name
+        self.page_content = self.fetch_page_content()
         self.press_releases = self.fetch_all_press_releases()
         self.titles = self.get_titles()
+        self.dates = self.get_dates()
+
+    def fetch_page_content(self) -> BeautifulSoup:
+        url_press = ""
+        if self.name == "Tesla":
+            url_press = tesl_url_press
+            selected_element_id = tesla_main_id
+        page = requests.get(url_press)
+        soup = BeautifulSoup(page.content, "html.parser")
+        results = soup.find(id=selected_element_id)
+
+        return results
 
     def fetch_all_press_releases(self) -> BeautifulSoup:
-        contents = BeautifulSoup()
 
         if self.name == "Tesla":
-            contents = helper.get_content(tesl_url_press, tesla_main_id)
+            page_content = self.page_content
             html_tag = tesla_press_releases[0]
             html_attr = tesla_press_releases[1]
             html_attr_val = tesla_press_releases[2]
 
-        press_releases = contents.find_all(html_tag, {html_attr: html_attr_val})
+        press_releases = page_content.find_all(html_tag, {html_attr: html_attr_val})
 
         return press_releases
 
@@ -45,6 +63,11 @@ class Company:
             titles.append(title)
 
         return titles
+
+    def get_dates(self):
+        dates = []
+
+        return dates
 
     def display_news(self) -> None:
         for i in range(len(self.press_releases)):
